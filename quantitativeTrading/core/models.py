9f3604bcc5c486c267bd09d08ff8a9479c1e19ef
Weslei Silva Bomfim <www.weslei@gmail.com>
2022-03-10 21:08:02 -0300

    :fire: Remove olds files
    remove Style.css and add setings

--- quantitativeTrading/core/models.py
+++ quantitativeTrading/core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Ibovespa(models.Model):
+    date = models.DateTimeField()
+    open = models.FloatField(null=True, blank=True)
+    high = models.FloatField(null=True, blank=True)
+    low = models.FloatField(null=True, blank=True)
+    close = models.FloatField(null=True, blank=True)
+    volume = models.FloatField(null=True, blank=True)
+    dividends = models.FloatField(null=True, blank=True)
+    stock = models.FloatField(null=True, blank=True)
+    splits = models.FloatField(null=True, blank=True)
+    def __str__(self):
+        return self.date
+
+class Dollar(models.Model):
+    date = models.DateTimeField()
+    open = models.FloatField(null=True, blank=True)
+    high = models.FloatField(null=True, blank=True)
+    low = models.FloatField(null=True, blank=True)
+    close = models.FloatField(null=True, blank=True)
+    volume = models.FloatField(null=True, blank=True)
+    dividends = models.FloatField(null=True, blank=True)
+    stock = models.FloatField(null=True, blank=True)
+    splits = models.FloatField(null=True, blank=True)
+    def __str__(self):
+        return self.date
