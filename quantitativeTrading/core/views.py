06eb0e239926b447f90b0d63a35cc34e0de3828a
Weslei Silva Bomfim <www.weslei@gmail.com>
2022-03-12 19:07:03 -0300

    :lipstick: Add layout
    sidebar and header

--- quantitativeTrading/core/views.py
+++ quantitativeTrading/core/views.py
@@ -1,10 +1,7 @@
-import yfinance as yf
+
 from django.shortcuts import render
 
 
 def index(request):
-    # ticket = "^BVSP"
-    # ibovespa = yf.Ticker(ticket)
-    # hist = ibovespa.history(period="max")
-    # return render(request, "core/index.html",{'cota': hist.to_dict()})
-    return render(request, "core/index.html")
+    info= {"title": "Quantitative Trading"}
+    return render(request, "core/home/index.html", {"info": info})
