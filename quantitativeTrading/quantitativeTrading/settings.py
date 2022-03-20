86df04993078270a82fc3d25aa6be2fc850a7010
Weslei Silva Bomfim <www.weslei@gmail.com>
2022-03-20 13:32:31 -0300

    :sparkles: Django Debug toolbar
    add toolbar debug

--- quantitativeTrading/quantitativeTrading/settings.py
+++ quantitativeTrading/quantitativeTrading/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar",
     "compressor",
     'fontawesomefree',
 ]
@@ -50,9 +51,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'quantitativeTrading.urls'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 TEMPLATES = [
     {
--- quantitativeTrading/quantitativeTrading/urls.py
+++ quantitativeTrading/quantitativeTrading/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
 
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
 
 
