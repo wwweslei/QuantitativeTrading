a74ea3e0da33007b46575fe35169921edc5fb024
Weslei Silva Bomfim <www.weslei@gmail.com>
2022-03-20 22:11:18 -0300

    :sparkles: Templates Updates
    add new fixtures the templates

--- quantitativeTrading/core/views.py
+++ quantitativeTrading/core/views.py
@@ -1,7 +1,16 @@
-
 from django.shortcuts import render
+from .models import Dollar, Ibovespa, Bitcoin, Smal, Xfix, SP_500, Nasdaq
 
 
 def index(request):
-    info= {"title": "Quantitative Trading"}
+    info = {
+        "title": "Quantitative Trading",
+        "wdo": Dollar.objects.latest("date"),
+        "ind": Ibovespa.objects.latest("date"),
+        "btc": Bitcoin.objects.latest("date"),
+        "smal": Smal.objects.latest("date"),
+        "sp": SP_500.objects.latest("date"),
+        "xfix": Xfix.objects.latest("date"),
+        "nasdaq": Nasdaq.objects.latest("date"),
+    }
     return render(request, "core/home/index.html", {"info": info})
