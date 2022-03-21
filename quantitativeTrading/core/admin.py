57a238d8705ee34cca8c7f6cd9e4cb5812db0220
Weslei Silva Bomfim <www.weslei@gmail.com>
2022-03-20 22:02:35 -0300

    :children_crossing: administration customization
    add new models to administration site

--- quantitativeTrading/core/admin.py
+++ quantitativeTrading/core/admin.py
@@ -1,8 +1,60 @@
+from re import search
 from django.contrib import admin
+from rangefilter.filters import DateRangeFilter
 
 # Register your models here.
 
-from .models import Ibovespa, Dollar
+from .models import Ibovespa, Dollar, Bitcoin, Smal, SP_500, Xfix, Nasdaq
 
-admin.site.register(Ibovespa)
-admin.site.register(Dollar)
+
+class base:
+    fields = (
+        "date",
+        "open",
+        "close",
+        "high",
+        "low",
+        "volume",
+        "dividends",
+        "stock_splits",
+    )
+    readonly_fields = fields
+    list_display = ("date", "open", "high", "low", "close", "volume")
+    date_hierarchy = "date"
+    search_fields = ["date"]
+    list_filter = [("date", DateRangeFilter), ("date")]
+
+
+@admin.register(Ibovespa)
+class IbovespaAdmin(base, admin.ModelAdmin):
+    pass
+
+
+@admin.register(Dollar)
+class DollarAdmin(base, admin.ModelAdmin):
+    pass
+
+
+@admin.register(Bitcoin)
+class BitcoinAdmin(base, admin.ModelAdmin):
+    pass
+
+
+@admin.register(Smal)
+class SmalAdmin(base, admin.ModelAdmin):
+    pass
+
+
+@admin.register(SP_500)
+class SP_500Admin(base, admin.ModelAdmin):
+    pass
+
+
+@admin.register(Xfix)
+class XfixAdmin(base, admin.ModelAdmin):
+    pass
+
+
+@admin.register(Nasdaq)
+class NadaqAdmin(base, admin.ModelAdmin):
+    pass
--- quantitativeTrading/quantitativeTrading/settings.py
+++ quantitativeTrading/quantitativeTrading/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "debug_toolbar",
     "compressor",
+    'rangefilter',
     'fontawesomefree',
 ]
 
