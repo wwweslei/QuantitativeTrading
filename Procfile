9ee247f60ea00d4d3c0358ffe690609175981c4c
Weslei Silva Bomfim <www.weslei@gmail.com>
2022-03-26 16:46:20 -0300

    :rocket: Config Deploy 1
    add elements the deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn quantitativeTrading.wsgi
--- quantitativeTrading/quantitativeTrading/settings.py
+++ quantitativeTrading/quantitativeTrading/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -143,3 +143,4 @@ COMPRESS_PRECOMPILERS = (
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
