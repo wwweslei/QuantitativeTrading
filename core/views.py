9df73df5356080a0fd1ec08d0f8c97d50a6128b0
Weslei Silva Bomfim <www.weslei@gmail.com>
2022-04-27 21:17:42 -0300

    feat(Models): introduces feature of updating the database automatically

--- core/views.py
+++ core/views.py
@@ -1,19 +1,18 @@
-from audioop import reverse
 from django.shortcuts import render
 from django.db import models
-
+from core.finance import download_market_data
 from .models import Dollar, Ibovespa, Bitcoin, Smal, Xfix, SP_500, Nasdaq
 
 
 def calc(obj: models.Model) -> dict:
-    """Calculate the percentage and variation of assets
+    """Calculate the percentage and variation of assets.
 
     Args:
         obj (models.Model): class object
 
     Returns:
         dict: dictionary with the percentage and last_value
-    """    
+    """
     query = obj.objects.all()[0:2]
     return {
         "pct": float(query[0].close / query[1].close - 1) * 100,
@@ -22,6 +21,15 @@ def calc(obj: models.Model) -> dict:
 
 
 def index(request):
+    """Render the index page.
+
+    Args:
+        request (_type_): request object
+
+    Returns:
+        _type_: render template index.html
+    """
+    download_market_data.run_all()
     card_info = {
         "wdo": calc(Dollar),
         "ind": calc(Ibovespa),
