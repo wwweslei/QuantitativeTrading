16c2c8a8268658d9e9d8d67e31bf0bb4f7f87e50
Weslei Silva Bomfim <www.weslei@gmail.com>
2022-05-24 20:53:06 -0300

    test(Views): :test_tube: Tests urls
    add tests ["/", "admin", "dolar"]

---
+++ core/tests/test_urls.py
@@ -0,0 +1,40 @@
+import pytest
+from django.test import Client
+
+
+@pytest.mark.django_db()
+def test_home_check(client: Client) -> None:
+    """This test ensures that home check is accessible."""
+    response = client.get('/')
+
+    assert response.status_code == 200
+
+
+def test_admin_unauthorized(client: Client) -> None:
+    """This test ensures that admin panel requires auth."""
+    response = client.get('/admin/')
+
+    assert response.status_code == 302
+
+
+def test_admin_authorized(admin_client: Client) -> None:
+    """This test ensures that admin panel is accessible."""
+    response = admin_client.get('/admin/')
+
+    assert response.status_code == 200
+
+
+def test_admin_docs_unauthorized(client: Client) -> None:
+    """This test ensures that admin panel docs requires auth."""
+    response = client.get('/admin/dollar/')
+
+    assert response.status_code == 302
+
+
+def test_admin_docs_authorized(admin_client: Client) -> None:
+    """This test ensures that admin panel docs are accessible."""
+    response = admin_client.get('/admin/core/')
+
+    assert response.status_code == 200
+    assert b'docutils' not in response.content  # type: ignore
+
