76fc42acc696a8a1a9b15aacda638f5c8f897ef2
weslei silva bonfim <www.weslei@gmail.com>
2023-06-10 19:32:53 -0300

    fix(Core): :bug: Update

--- quantitativeTrading/settings.py
+++ quantitativeTrading/settings.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from pathlib import Path
 
-import django_heroku
 from decouple import Csv, config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "compressor",
+    "django_sass_compiler",
     "rangefilter",
     "crispy_forms",
     "crispy_bootstrap5",
@@ -154,17 +154,17 @@ STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles"))
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "sass_processor.finders.CssFinder",
     "compressor.finders.CompressorFinder",
 )
 
 COMPRESS_ENABLED = True  # ou not DEBUG
-COMPRESS_PRECOMPILERS = (("text/x-sass", "sassc {infile} {outfile}"),)
+COMPRESS_PRECOMPILERS = (("text/x-scss", "django_libsass.SassCompiler"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-django_heroku.settings(locals())
 
 LOGGING = {
     "version": 1,
